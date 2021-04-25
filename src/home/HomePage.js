c87242e063802ea5bddc2e8caf1dc9068829e78b
Melody Phu <melody.phu.8@gmail.com>
2021-04-25 13:36:44 -0400

    Created all skeleton files

---
+++ src/home/HomePage.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+
+class HomePage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      
+    }
+  }
+
+  // called the first time the component is rendered by App.js
+  componentDidMount() {
+    this.setState({
+
+    })
+  }
+
+  render() {
+    return (
+      <div> 
+        This is the Home Page.
+      </div>
+    );
+  }
+}
+
+export default HomePage;
---
+++ src/summary/ExpressionScore.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+
+class ExpressionScore extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      
+    }
+  }
+
+  // called the first time this component is rendered
+  componentDidMount() {
+    this.setState({
+
+    })
+  }
+
+  render() {
+    return (
+      <div> 
+        This is the Expression Score summary.
+      </div>
+    );
+  }
+}
+
+export default ExpressionScore;
