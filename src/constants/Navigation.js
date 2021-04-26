038c21fc056f05e4b010b9b2965ddabdbca5d594
Melody Phu <melody.phu.8@gmail.com>
2021-04-26 19:15:50 -0400

    finished file upload, next up is parsing

--- src/constants/Navigation.js
+++ src/constants/Navigation.js
@@ -5,3 +5,9 @@ export const SCREENS = {
     practice: 2,
     summary: 3
 }
+
+export const UPLOAD_STEPS = {
+    upload: 0,
+    parse: 1,
+    select: 2,
+}
--- src/upload/UploadArena.js
+++ src/upload/UploadArena.js
@@ -1,52 +1,138 @@
 import React from 'react';
+import RootRef from '@material-ui/core/RootRef';
 
-import {SCREENS} from './../constants/Navigation';
+import {SCREENS, UPLOAD_STEPS} from './../constants/Navigation';
 
-import Button from '@material-ui/core/Button';
-import CancelIcon from '@material-ui/icons/Close';
+import {DropzoneArea} from 'material-ui-dropzone';
+import {Button, CircularProgress} from '@material-ui/core';
+
+import HomeIcon from '@material-ui/icons/Home';
 import NextIcon from '@material-ui/icons/ArrowForward';
+import UploadIcon from "@material-ui/icons/Publish";
+import CancelIcon from "@material-ui/icons/Clear";
 
+// check out https://yuvaleros.github.io/material-ui-dropzone/ for documentation
 class UploadArena extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      
+      files: [],
+      step: UPLOAD_STEPS.upload,
+    };
+    this.fileRef = React.createRef();
+  }
+
+  // for uploading files aka step 0
+  updateFiles = files => {
+    this.setState({
+      files: files,
+    })
+  }
+
+  // if a user wants to cancel
+  cancelFileUpload = () => {
+    this.setState({
+      step: UPLOAD_STEPS.upload,
+    });
+  }
+
+  // for processing the file aka step 1
+  processText = () => {
+    this.setState({
+      step: UPLOAD_STEPS.parse,
+    });
+  }
+
+  getNavButtons() {
+    switch (this.state.step) {
+      case UPLOAD_STEPS.upload:
+        return (
+          <div>
+            <Button
+              variant="contained"
+              color="primary"
+              size="large"
+              endIcon={<HomeIcon />}
+              onClick={() => this.props.switchScreen(SCREENS.home)}
+            >
+              Home
+            </Button>
+            <Button
+              variant="contained"
+              color="primary"
+              size="large"
+              endIcon={<UploadIcon />}
+              disabled={this.state.files.length != 1}
+              onClick={() => this.processText()}
+            >
+              Upload
+            </Button>
+          </div>
+        );
+      case UPLOAD_STEPS.parse:
+        return (
+          <div>
+            <Button
+              variant="contained"
+              color="primary"
+              size="large"
+              endIcon={<CancelIcon />}
+              onClick={() => this.cancelFileUpload()}
+            >
+              Cancel
+            </Button>
+          </div>
+        );
+      case UPLOAD_STEPS.select:
+        return (
+          <div>
+            <Button
+              variant="contained"
+              color="primary"
+              size="large"
+              endIcon={<NextIcon/>}
+              onClick={() => this.props.switchScreen(SCREENS.practice)}
+            >
+              Begin
+            </Button>
+          </div>
+        )
     }
   }
 
-  // called the first time this component is rendered
-  componentDidMount() {
-    this.setState({
-
-    })
+  getArena() {
+    switch (this.state.step) {
+      case UPLOAD_STEPS.upload:
+        return (
+          <div> 
+            <DropzoneArea
+              onChange={(files) => this.updateFiles(files)}
+              acceptedFiles={['.txt']}
+              filesLimit={1}
+              dropzoneText={"Drag and drop your script here (.txt files only) or click"}
+            />
+          </div>
+        );
+      case UPLOAD_STEPS.parse:
+        return (
+          <div>
+            <CircularProgress />
+            Your file is being processed.
+          </div>
+        );
+      case UPLOAD_STEPS.select:
+        return (
+          <div></div>
+        );
+    }
   }
 
   render() {
     return (
       <div>
-        <div> 
-          This is the Upload Arena screen.
-        </div>
-        <Button
-          variant="contained"
-          color="primary"
-          size="large"
-          endIcon={<CancelIcon />}
-          onClick={() => this.props.switchScreen(SCREENS.home)}
-        >
-          Cancel
-        </Button>
-        <Button
-          variant="contained"
-          color="primary"
-          size="large"
-          endIcon={<NextIcon />}
-          onClick={() => this.props.switchScreen(SCREENS.practice)}
-        >
-          Begin
-        </Button>
+        {this.getArena()}
+        {this.getNavButtons()}
       </div>
-
     );
   }
 }
